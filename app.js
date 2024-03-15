2bf606c13cc71ab55f82df60ff9b03ee9476ca2e
pranavaselva <pranava.m@kalvium.community>
2024-03-15 12:52:47 +0530

    react 2

---
+++ app.js
@@ -0,0 +1,9 @@
+// Functional component
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
+}
+// Render the Container
+const container = document.getElementById('react-container');
+ReactDOM.render(React.createElement(Container),container);
